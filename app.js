448ad286a2b66fe0f59173b79c0b17ceae95fb82
Jonathan <jcunanan05@gmail.com>
2018-05-10 14:18:03 +0000

    Finish blog and styling
    Added Edit, Update, and Delete route and finsih styling

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express'),
     app = express(),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
+    methodOverride = require('method-override'),
+    expressSanitizer = require('express-sanitizer');
+    bodyParser = require('body-parser'), /* global bodyParser */
+    mongoose = require('mongoose'); /* global mongoose */
     
 //app config
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
 //Model config
@@ -52,6 +56,8 @@ app.get('/blogs/new', (req, res) => {
 
 //Create Route
 app.post('/blogs', (req, res) => {
+  //sanitize blog
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   //create blog
   Blog.create(req.body.blog, (err, newBlog) => {
     if (err) {
@@ -74,6 +80,47 @@ app.get('/blogs/:id', (req, res) => {
     
     //success
     res.render('show', {blog: foundBlog});
+  });
+});
+
+//edit route
+app.get('/blogs/:id/edit', (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect('/blogs');
+      return;
+    }
+    
+    //success
+    res.render('edit', {blog: foundBlog});
+  });
+});
+
+//update route
+app.put('/blogs/:id', (req, res) => {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  const newBlog = req.body.blog;
+  
+  Blog.findByIdAndUpdate(req.params.id, newBlog, (err, updatedBlog) => {
+    if (err) {
+      res.redirect('/blogs');
+      return;
+    }
+    
+    //success
+    res.redirect(`/blogs/${req.params.id}`);
+  });
+});
+
+//delete route
+app.delete('/blogs/:id', (req, res) => {
+  Blog.findByIdAndRemove(req.params.id, err => {
+    if (err) {
+      res.redirect('/blogs');
+      return;
+    }
+    
+    res.redirect('/blogs');
   });
 });
 
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Material Blog
 
+![Material Blog](/public/img/material.png)
+
 This is sample blog to enhance and solidfy the concept of REST using Express, and MongoDB. It uses Material UI for the design.
